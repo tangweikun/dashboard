56b977e8e1b0fb3eb8426510b639d74d0483cc18
sherotree <wytreeshero@163.com>
2024-03-28 21:45:15 +0800

    Add @vercel/analytics package to layout.tsx and package.json

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 import { Metadata } from 'next';
- 
+import { Analytics } from '@vercel/analytics/react';
+
 export const metadata: Metadata = {
   title: {
     template: '%s | Acme Dashboard',
@@ -18,7 +19,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
